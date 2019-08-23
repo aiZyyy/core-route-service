d5bd708c2b00fd55d74b99a00682917351e33b17
MiaoWoo <admin@yumc.pw>
2019-08-23 10:38:15 +0800

    Create & Init core-route-service Project...

---
+++ src/main/java/com/sixi/core/routeservice/CoreRouteServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sixi.core.routeservice;
+
+import org.springframework.boot.SpringApplication;
+
+import com.sixi.micro.common.annotation.SixiMicroServiceApplication;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author MiaoWoo
+ */
+@SixiMicroServiceApplication
+public class CoreRouteServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreRouteServiceApplication.class, args);
+    }
+}
